a0cd8c0fb5dac9b4f911fcb4f2d4bb88d3a875c3
Ennin Jeter <jeterennin@gmail.com>
2022-04-20 16:13:01 -0500

    Created EntriesChartTacker

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import DisplayEntries from './Components/DisplayEntries/DisplayEntries';
 import AddEntryForm from './Components/AddEntry/AddEntryForm';
-
+import EntriesChartTracker from './Components/EntriesChartTracker/EntriesChartTracker';
 
 
 function App() {
@@ -18,8 +18,9 @@ function App() {
   
   return (
     <div>
-      <DisplayEntries parentEntries={entries}/>
+      <DisplayEntries parentEntries={entries} />
       <AddEntryForm addNewEntry={addNewEntry} />
+      <EntriesChartTracker parentEntries={entries} />
       
     
     </div>
